c83412a7ae9e9128416c97ff0b9bdd1c3dc007f3
Michael Li <limichael1099419@gmail.com>
2018-12-21 21:00:03 -0800

    Got restaurant names

--- yelp_scrape.py
+++ yelp_scrape.py
@@ -1,16 +1,21 @@
-import request #connects to the webpage , which returns to the html 
+import requests #connects to the webpage , which returns to the html 
 import urllib.request
 import bs4
 from bs4 import BeautifulSoup as soup 
 
 url_full = "https://www.yelp.com/search?find_desc=&find_loc=cal+state+fullerton&ns=1"
 
-website = request.get(url_full);
+website = requests.get(url_full);
 
-ParsedObject = soup(website.text, "html.parsers")
+ParsedObject = soup(website.text, "html.parser")
 
-distance_search = "<span class="domtags--span__373c0__1VGzF">0.7 Miles</span>"
+ParsedObject1 = soup(website.text, "html.parser")
 
-x = soup.find ("span", "domtags--span__373c0__1VGzF")
+#distance_search = <span class="domtags--span__373c0__1VGzF">0.7 Miles</span>
 
-print(x)
+x = ParsedObject.find_all ("a", "lemon--a__373c0__1_OnJ link__373c0__29943 link-color--blue-dark__373c0__1mhJo link-size--inherit__373c0__2JXk5")
+
+y = ParsedObject.find_all ("h3", "lemon--h3__373c0__5Q5tF heading--h3__373c0__1n4Of alternate__373c0__1uacp")
+
+for i in y: 
+	print(i.text)
